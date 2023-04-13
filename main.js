df3395a3520f1171eed7d4eaadce2f35633c6e52
Michael Lanius Friedrich <michaelfriedrich253@gmail.com>
2023-04-13 18:24:30 -0300

    updated login and register

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 const express = require('express');
-const { registerUser, getAllUsers } = require('./user');
+const { registerUser, getAllUsers, login } = require('./user');
 const { getMessage, sendMessage } = require('./message');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+const JWT_SECRET = process.env.JWT_SECRET;
 
 const app = express();
 const port = 3000;
@@ -8,31 +12,38 @@ const port = 3000;
 app.use(express.json());
 
 app.post('/register', async (req, res) => {
-  const { username, name, password } = req.body
+  const { username, name } = req.body
+  let { password } = req.body
+  password = jwt.sign(password, JWT_SECRET);
   const user = await registerUser(username, name, password);
-  res.json(user);
+  if (!user) {
+    return res.status(500).send({ error: 'User already exists' });
+  }
+  return res.json(user);
 });
 
 app.post('/login', async (req, res) => {
-  const { username, password } = req.body
+  const { username } = req.body
+  let { password } = req.body
+  password = jwt.sign(password, JWT_SECRET);
   const user = await login(username, password);
-  res.json(user);
+  return res.json(user);
 });
 
 app.get('/users', async (req, res) => {
   const users = await getAllUsers();
-  res.json(users);
+  return res.json(users);
 });
 
 app.get('/messages', async (req, res) => {
   const messages = await getMessage();
-  res.json(messages);
+  return res.json(messages);
 });
 
 app.post('/sendMessage', async (req, res) => {
   const { authorId, message } = req.body;
   const response = await sendMessage(authorId, message);
-  res.json(response);
+  return res.json(response);
 });
 
 app.listen(port, () => {
